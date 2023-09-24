76b60afe216e7f8b960c0358edb8c9fa86957a4e
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-24 04:35:14 +0300

    entrypoint for starting the application

---
+++ index.js
@@ -0,0 +1,30 @@
+
+require('express-async-errors')
+const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./server/app')
+const cors = require('cors')
+const { MONGODB_URI, PORT, ALLOWED_ORIGINS } = require('./server/utils/config')
+
+
+app.use(cors(
+  {
+    origin: ALLOWED_ORIGINS,
+  }
+))
+
+mongoose.connect(MONGODB_URI)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`)
+})
