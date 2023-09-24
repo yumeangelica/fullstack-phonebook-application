438b07db274cdfc938c82737312ebf5b39a6fb3e
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-24 04:37:15 +0300

    add config file

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'es6': true,
+    'jest/globals': true,
+    'cypress/globals': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react', 'jest', 'cypress'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-undef': 'warn',
+    'no-unused-vars': 'warn',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 'off',
+    'react/prop-types': 0
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: [
+    './client'
+  ],
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'main.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'css-loader',
+          },
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.*', '.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html'
+    }),
+    new Dotenv({
+      path: './.env',
+    }),
+  ],
+
+
+}
+
