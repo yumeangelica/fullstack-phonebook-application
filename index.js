373dfcbd577b035110f9a90dc6639d8095611e30
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-07 08:34:28 +0200

    modernize syntax

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 require('express-async-errors');
 const mongoose = require('mongoose');
 const http = require('http');
@@ -6,19 +5,20 @@ const app = require('./server/app');
 const cors = require('cors');
 const { MONGODB_URI, PORT, ALLOWED_ORIGINS } = require('./server/utils/config');
 
-app.use(cors(
-  {
-    origin: ALLOWED_ORIGINS,
-  },
-));
+app.use(cors({
+  origin: ALLOWED_ORIGINS,
+}));
 
-mongoose.connect(MONGODB_URI)
-  .then((result) => {
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
     console.log('connected to MongoDB');
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log('error connecting to MongoDB:', error.message);
-  });
+  }
+};
+
+connectToMongoDB();
 
 const server = http.createServer(app);
 
