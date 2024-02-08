a263652b9fa056ca87e92c92654b538677329160
yumeangelica <74900808+yumeangelica@users.noreply.github.com>
2024-02-08 19:54:47 +0200

    update readability and placeholder

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -2,11 +2,18 @@ const Filter = ({ handleFilterChange, newFilter }) => {
   return (
     <div className="filter-container">
       <div className="filter-content">
-        <label className="filter-label">Filter shown with</label>
-        <input className="filter-input" onChange={handleFilterChange} value={newFilter} />
+        <label htmlFor="filterInput" className="filter-label">Filter shown with</label>
+        <input
+          id="filterInput"
+          type="text"
+          className="filter-input"
+          onChange={handleFilterChange}
+          value={newFilter}
+          placeholder="Enter name or number"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
